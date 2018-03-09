6fd2b2d76bea02d56ea37f57d09c69f8db870568
Elliott Marquez <emarquez@google.com>
2018-03-08 16:08:34 -0800

    auto-converted by polymer-modulizer

--- test/disabled-input.js
+++ test/disabled-input.js
@@ -1,4 +1,7 @@
-<!--
+import '../../polymer/polymer.js';
+import { Polymer } from '../../polymer/lib/legacy/polymer-fn.js';
+import { html } from '../../polymer/lib/utils/html-tag.js';
+/**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,29 +9,23 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-
-<link rel="import" href="../../polymer/polymer.html">
-
-<dom-module id="disabled-input">
-  <template>
+*/
+Polymer({
+  _template: html`
     <iron-input bind-value="{{myValue}}" invalid="{{myInvalid}}" id="input">
-      <input disabled>
+      <input disabled="">
     </iron-input>
-  </template>
-</dom-module>
+`,
 
-<script>
-  Polymer({
-    is: 'disabled-input',
-    properties: {
-      myValue: {
-        value: 'foo'
-      },
+  is: 'disabled-input',
 
-      myInvalid: {
-        value: false
-      }
+  properties: {
+    myValue: {
+      value: 'foo'
+    },
+
+    myInvalid: {
+      value: false
     }
-  });
-</script>
+  }
+});
